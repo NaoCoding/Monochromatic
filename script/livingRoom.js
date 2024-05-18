8da7a18cf3393553512cba6e71bd3faf12411eeb
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-05-18 20:05:04 +0800

    Update Living room

--- script/livingRoom.js
+++ script/livingRoom.js
@@ -1,6 +1,7 @@
 let livingroom_bigTable,livingroom_door2bed
 let livingroom_door2outside,livingroom_door2parent
 let livingroom_flower,livingroom_knife
+let livingroom_photo,livingroom_sova,livingroom_tv
 
 function start_SceneLivingRoom(){
     
@@ -8,12 +9,13 @@ function start_SceneLivingRoom(){
     FlashLightWhite_getXY()
 
     MoveBorder = ["livingroom_bigTable","livingroom_door2bed","livingroom_door2outside","livingroom_door2parent"
-                    ,"livingroom_flower"]
-    PressEBorder = [0,1,2,3,4]
+                    ,"livingroom_flower","livingroom_knife","livingroom_photo","livingroom_sova",
+                    "livingroom_tv"]
+    PressEBorder = [0,1,2,3,4,5,6,7,8]
     WallBorder = [15,85,15,85]
 
     livingroom_bigTable = createImg("image/livingroom_bigTable.png","bed")
-    livingroom_bigTable.style("position:absolute;top:32%;left:53%;width:17%;height:25%;");
+    livingroom_bigTable.style("position:absolute;top:35%;left:53%;width:17%;height:25%;");
     livingroom_bigTable.attribute("id","livingroom_bigTable")
     livingroom_bigTable.show()
 
@@ -41,6 +43,23 @@ function start_SceneLivingRoom(){
     livingroom_knife.style("position:absolute;top:30%;left:28%;width:10%;height:18%;");
     livingroom_knife.attribute("id","livingroom_knife")
     livingroom_knife.show()
+
+    livingroom_photo = createImg("image/livingroom_photo_label.png","bed")
+    livingroom_photo.style("position:absolute;top:50%;left:82%;width:3%;height:12%;");
+    livingroom_photo.attribute("id","livingroom_photo")
+    livingroom_photo.show()
+
+    livingroom_sova = createImg("image/livingroom_sova.png","bed")
+    livingroom_sova.style("position:absolute;top:25%;left:40%;width:7%;height:28%;");
+    livingroom_sova.attribute("id","livingroom_sova")
+    livingroom_sova.show()
+
+    livingroom_tv
+
+    livingroom_tv = createImg("image/livingroom_tv.png","bed")
+    livingroom_tv.style("position:absolute;top:30%;left:15%;width:4%;height:18%;");
+    livingroom_tv.attribute("id","livingroom_tv")
+    livingroom_tv.show()
 
 
 
